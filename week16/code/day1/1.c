45504d51e747f6a73a31424f135586676781f29b
gobi <1531950593@qq.com>
2019-12-23 05:40:15 -0800

    week16 code

---
+++ week16/code/day1/1.c
@@ -0,0 +1,36 @@
+#include "my.h"
+int main()
+{
+	int sockfd, client_fd;
+	struct sockaddr_in my_addr;
+	struct sockaddr_in remote_addr;
+	int sin_size;
+	if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+	{
+		perror("socket 创建失败！\n");
+		exit(1);
+	}
+	my_addr.sin_family = AF_INET;
+	my_addr.sin_port = htons(SERVPORT);
+	my_addr.sin_addr.s_addr = INADDR_ANY;
+	bzero(&(my_addr.sin_zero),8);
+	
+	if(bind(sockfd,(struct sockaddr *)&my_addr,sizeof(struct sockaddr))==-1)
+	{
+		perror("bind 出错!\n");
+		exit(1);
+	}
+	if(listen(sockfd,BACKLOG)==-1)
+	{
+		perror("listen 出错!\n")
+	}
+	while(1)
+	{
+		sin_size = sizeof(struct sockaddr_in);
+		if(client_fd = accept(sockfd, (struct sockaddr *)&remote_addr,&sin_size)==-1)
+		{
+			perror("accept error\n");
+			cotinue;
+		}
+	}
+}
